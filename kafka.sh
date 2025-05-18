ae2ef4c6686b2337d2d99bd9bf92fa02594e9e0c
sofia19c <sofivampaireknight@gmail.com>
2025-05-17 21:57:52 -0500

    Crear archivo comandos de kafka para el topic

---
+++ kafka.sh
@@ -0,0 +1,3 @@
+kafka-topics.sh --create --topic embarazo-registrado --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 
+kafka-topics.sh --create --topic ciclo-registrado --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+kafka-topics.sh --create --topic usuario-creado --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
